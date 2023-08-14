423ceda4882d25cf2f58f24efa3c7ec5b6be9e9f
andrea-ap <63066393+andrea-ap@users.noreply.github.com>
2023-08-14 22:20:36 +0200

    Users.py

--- nut/Users.py
+++ nut/Users.py
@@ -14,9 +14,6 @@ class User:
 		self.requireAuth = True
 		self.switchHost = None
 		self.switchPort = None
-        
-        # Logged
-        self.isLoggedIn = False
 
 	def loadCsv(self, line, map=[]):
 		split = line.split('|')
@@ -97,15 +94,6 @@ def auth(id, password, address):
 		return None
 
 	user = users[id]
-    
-    
-    # Check if the user is already logged in
-#if user.isLoggedIn:
-#return None
-
-
-
-
 
 	if user.requireAuth == 0 and address == user.remoteAddr:
 		return user
@@ -116,26 +104,8 @@ def auth(id, password, address):
 	# TODO: save password hash in config
 	if user.password != password:
 		return None
-        
-        
-        
-     # Set the isLoggedIn attribute to True to indicate that the user is now logged user.isLoggedIn = True
-     #user.isLoggedIn = True
-     
-     
-     
 
 	return user
-    
-    #You will also need to add a way to log users out
-    
-    #def logout(user):
-#user.isLoggedIn = False
-
-
-
-    
-    
 
 def load(path='conf/users.conf'):
 	global users
