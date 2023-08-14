f0b9faa9de00b027574333603b3b62f2c69b3c8c
andrea-ap <63066393+andrea-ap@users.noreply.github.com>
2023-08-14 16:50:04 +0200

    Users.py

--- nut/Users.py
+++ nut/Users.py
@@ -14,7 +14,7 @@ class User:
 		self.requireAuth = True
 		self.switchHost = None
 		self.switchPort = None
-        self.isLoggedIn = False  
+       # self.isLoggedIn = False  
         
         
         
@@ -119,8 +119,8 @@ def auth(id, password, address):
 
 
 
-#def logout(user):
-    #user.isLoggedIn = False
+def logout(user):
+    user.isLoggedIn = False
 
 
 
