1fee5a6d3b253dc4e04ed7da80379c5ab5f48518
andrea-ap <63066393+andrea-ap@users.noreply.github.com>
2023-08-16 12:39:18 +0200

    Users.py

--- nut/Users.py
+++ nut/Users.py
@@ -107,9 +107,9 @@ def auth(id, password, address):
     if user.password != password:
         return None
 
-    # Check if the user is already connected quando si connette utente 1 in conteggio permanente 
+    # Check if the user is already connected inserisci qui conteggio
     if id in connected_users:
-        if session_counts.get(id, 0) >= 3:
+        if session_counts.get(id, 0) >= 5:
             # User has reached the maximum number of sessions
             return None
     else:
